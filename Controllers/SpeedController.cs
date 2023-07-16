9ad117a19e9d26b93293d9dafd3ae6f9fb3ad1fd
saurabhkapure <saurabhkapure2000@gmail.com>
2023-07-16 12:26:26 +0530

    Add project files.

---
+++ Controllers/SpeedController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class SpeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Calculate(SpeedModel model)
+        {
+            
+            model.KilometersPerHour = model.MetersPerSecond * 3.6;
+
+           
+            ViewBag.SpeedResult = model.KilometersPerHour;
+
+            return View("Index");
+        }
+    }
+}
---
+++ Models/SpeedModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.Models
+{
+    public class SpeedModel
+    {
+        public double MetersPerSecond { get; set; }
+        public double KilometersPerHour { get; set; }
+    }
+}
